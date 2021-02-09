5f10dbc84bd0c2e000ed3c2ce45254e4ea9eda15
vikash <vikashsonu1995@gmail.com>
2021-02-09 13:54:38 +0530

    first Poc

---
+++ Poc_SpringBoot/src/main/java/com/nt/PocSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringBootApplication.class, args);
+	}
+
+}
